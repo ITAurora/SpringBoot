d7e0cad9d92b01477315dc768128e995f59f9097
ITAurora <691871826@qq.com>
2025-02-13 17:20:50 +0800

    init

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    // 可以在这里定义自定义查询方法
+}
